9907b51adf31ba5e597ad530b33576db7c475c5e
Adel Lassag <adetech.aadl@gmail.com>
2020-06-29 21:02:24 +0100

    feat: Render show-time view

--- products/views.py
+++ products/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
 
 
 def say_hi(request, name):
     return render(request, 'say-hi.html', {'name': name})
+
+
+def show_time(request):
+    now = timezone.now()
+    return render(request, 'show-time.html', {'now': now})
--- store/urls.py
+++ store/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import say_hi
+from products.views import say_hi, show_time
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('say-hi/<str:name>', say_hi),
+    path('show-time/', show_time),
 ]
