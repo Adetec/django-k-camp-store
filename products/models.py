d2fdb326642b7ae31e902450818c2ec7bf8c775b
Adel Lassag <adetech.aadl@gmail.com>
2020-06-30 18:35:06 +0100

    feat: Define Product model in admin panel

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.title
