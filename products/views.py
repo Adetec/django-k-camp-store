037201ba5becc1073acae9cdebe538b8da0fcb47
Adel Lassag <adetech.aadl@gmail.com>
2020-06-30 19:23:38 +0100

    feat: Create product list view function

--- products/views.py
+++ products/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from django.utils import timezone
+from .models import Product
 
 # Create your views here.
 
 
-def say_hi(request, name):
-    return render(request, 'say-hi.html', {'name': name})
-
-
-def show_time(request):
-    now = timezone.now()
-    return render(request, 'show-time.html', {'now': now})
+def products_list(request):
+    products = Product.objects.all()
+    return render(request, 'products-list.html', {'products': products})
