b7fbd604fef6211982d1706a8041caf0ac0a17a7
Adel Lassag <adetech.aadl@gmail.com>
2020-06-30 20:15:37 +0100

    fix: Change description from tex to char field

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.CharField(max_length=500)
 
 
     def __str__(self):
