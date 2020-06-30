9bcbf7238efa330648711d429d01f27b09a7354d
Adel Lassag <adetech.aadl@gmail.com>
2020-06-30 19:24:21 +0100

    feat: Add product list template's url

--- store/urls.py
+++ store/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import say_hi, show_time
+from products.views import products_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', products_list),
 ]
