a00b4e3751fd02333380a2a246f582d579adb331
Adel Lassag <adetech.aadl@gmail.com>
2020-07-06 21:47:50 +0100

    feat: Add Category model

--- products/models.py
+++ products/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+
+    def __str__(self):
+        return self.title 
+
 
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     price = models.IntegerField(default=0)
