6cd8ec6fa6227eae853ab33b160942019a6c3681
Adel Lassag <adetech.aadl@gmail.com>
2020-07-06 19:05:06 +0100

    feat: Add price to Product model

--- products/models.py
+++ products/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
+    price = models.IntegerField(default=0)
 
 
     def __str__(self):
