d9bf1a7bc2d2365039c76745dca313e5dfb2cb70
Adel Lassag <adetech.aadl@gmail.com>
2020-07-07 20:21:12 +0100

    feat: Create product add view

--- products/views.py
+++ products/views.py
@@ -14,3 +14,7 @@ def product_details(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {'product': product}
     return render(request, 'products/product-details.html', context)
+
+
+def product_add(request):
+    return render(request, 'products/product-add.html')
