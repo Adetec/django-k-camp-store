1b32ebff8c2f4c065751480106e4047943a26696
Adel Lassag <adetech.aadl@gmail.com>
2020-07-07 20:23:39 +0100

    feat: Create add product url

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import products_list, product_details
+from .views import products_list, product_details, product_add
 
 urlpatterns = [
     path('products/', products_list, name='products_list'),
-    path('products/<pk>', product_details, name='product_details')
+    path('products/<pk>', product_details, name='product_details'),
+    path('products/add', product_add, name='product_add')
 ]
