473fd1b0d36585af15d81cd89dcf7552e3d5d8e6
Adel Lassag <adetech.aadl@gmail.com>
2020-07-07 21:52:55 +0100

    feat: Complete add product functionalities

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,5 @@ from .views import products_list, product_details, product_add
 urlpatterns = [
     path('products/', products_list, name='products_list'),
     path('products/<pk>', product_details, name='product_details'),
-    path('products/add', product_add, name='product_add')
+    path('products/new/add', product_add, name='product_add')
 ]
