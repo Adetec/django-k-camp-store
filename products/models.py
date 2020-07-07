723dbeece79b90b449ec5609527aeac74e90d3de
Adel Lassag <adetech.aadl@gmail.com>
2020-07-07 19:43:54 +0100

    feat: Add brand to product model

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Product(models.Model):
+    brand = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     price = models.IntegerField(default=0)
