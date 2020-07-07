44b8b24300bc35b11069814e40d69decab6ba9fe
Adel Lassag <adetech.aadl@gmail.com>
2020-07-07 23:03:36 +0100

    fix: Reorder products urls

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from .views import products_list, product_details, product_add, product_edit
 
 urlpatterns = [
     path('products/', products_list, name='products_list'),
+    path('products/add', product_add, name='product_add'),
+    path('products/edit/<pk>', product_edit, name='product_edit'),
     path('products/<pk>', product_details, name='product_details'),
-    path('products/new/add', product_add, name='product_add'),
-    path('products/edit/<pk>', product_edit, name='product_edit')
 ]
