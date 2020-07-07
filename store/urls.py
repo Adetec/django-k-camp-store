6c244f52b9e2e0e6de05ab43cd3d42ac556fb2aa
Adel Lassag <adetech.aadl@gmail.com>
2020-07-07 23:04:40 +0100

    fix: Remove redirect functionality

--- store/urls.py
+++ store/urls.py
@@ -15,14 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
-
-def home(request):
-    return redirect('product_list')
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
     path('', include('products.urls'))
 ]
