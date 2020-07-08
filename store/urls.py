6d99a9b42af9e9cb200695cd71fd3b1e64abe0bc
Adel Lassag <adetech.aadl@gmail.com>
2020-07-08 16:26:47 +0100

    feat: Use redirect method

--- store/urls.py
+++ store/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def home(request):
+    return redirect('products_list')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('', include('products.urls'))
 ]
