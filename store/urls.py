57eff384d5f995bd28b31aa7d82f7c362060c3f7
Adel Lassag <adetech.aadl@gmail.com>
2020-07-09 20:12:31 +0100

    feat: Add static settings to store's urls

--- store/urls.py
+++ store/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 def home(request):
@@ -26,4 +28,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('', include('products.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
