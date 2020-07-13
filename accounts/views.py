cd3dcdcd009f49ef966bf75998c638abbae094c3
Adel Lassag <adetech.aadl@gmail.com>
2020-07-13 18:22:10 +0100

    feat: Create signup view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(resquest.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'registration/signup.html')
