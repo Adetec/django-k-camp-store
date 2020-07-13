e1631eb88a90cde2da6c9f0283ef3a866b8ac002
Adel Lassag <adetech.aadl@gmail.com>
2020-07-13 20:22:50 +0100

    fix: Typo resquest

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from .forms import SignUpForm
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        form = SignUpForm(resquest.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
