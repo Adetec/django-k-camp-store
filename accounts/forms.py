7d9766dadba349c97f6193b512b7d5fb372b0ee8
Adel Lassag <adetech.aadl@gmail.com>
2020-07-13 18:08:23 +0100

    feat: Create signup form

---
+++ accounts/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django import forms
+
+
+user = get_user_model
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+    email = forms.EmailField(max_lenth=254, help_text='Required! Inform a valid email address')
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'password1',
+            'password2',
+            'first_name',
+            'last_name',
+            'email'
+        )
