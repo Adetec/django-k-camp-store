e77cd8fe56718aab57b8762c930d54ef9196942f
Adel Lassag <adetech.aadl@gmail.com>
2020-07-13 19:35:10 +0100

    feat: Add login & logout views urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import signup
 
 urlpatterns = [
     path('signup', signup, name='signup'),
+    path('login', auth_views.LoginView, name='login'),
+    path('logout', auth_views.LogoutView, name='logout')
 ]
