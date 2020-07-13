16807736ae85cd208b109a5bd63fb812624e6049
Adel Lassag <adetech.aadl@gmail.com>
2020-07-13 18:54:13 +0100

    fix: Fix typo (email max length)

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,7 +9,7 @@ user = get_user_model
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
-    email = forms.EmailField(max_lenth=254, help_text='Required! Inform a valid email address')
+    email = forms.EmailField(max_length=256, help_text='Required! Inform a valid email address')
 
     class Meta:
         model = User
