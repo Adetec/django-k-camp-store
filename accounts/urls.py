fc0fbbeaacf15ba4d368192f236c16b6f3f3334a
Adel Lassag <adetech.aadl@gmail.com>
2020-07-13 19:49:02 +0100

    fix: Add asveiw method

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from .views import signup
 
 urlpatterns = [
     path('signup', signup, name='signup'),
-    path('login', auth_views.LoginView, name='login'),
-    path('logout', auth_views.LogoutView, name='logout')
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout')
 ]
