ac1317517912a05d3c62601344b0e27be45deeda
Adel Lassag <adetech.aadl@gmail.com>
2020-07-13 18:56:20 +0100

    fix: Add missing parentheses

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django import forms
 
 
-User = get_user_model
+User = get_user_model()
 
 
 class SignUpForm(UserCreationForm):
--- store/urls.py
+++ store/urls.py
@@ -27,5 +27,6 @@ def home(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('', include('products.urls'))
+    path('', include('products.urls')),
+    path('accounts/', include('accounts.urls')) 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
