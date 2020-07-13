ae602f07ab2e74b4e85745cc90e05deb15a2dc92
Adel Lassag <adetech.aadl@gmail.com>
2020-07-13 20:48:59 +0100

    fix: Add slashes to accounts' urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from .views import signup
 
 urlpatterns = [
-    path('signup', signup, name='signup'),
-    path('login', auth_views.LoginView.as_view(), name='login'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout')
+    path('signup/', signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
