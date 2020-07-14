8738a6f3bb6a60f397980686082ef9955a028967
Adel Lassag <adetech.aadl@gmail.com>
2020-07-14 17:49:15 +0100

    feat: Create profile using django signals

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=500)
+    address = models.CharField(max_length=500, default='')
 
 
     def __str__(self):
