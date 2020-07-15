0651677e8cbb53da6f65aaca8a86ac8a5acc1868
Adel Lassag <adetech.aadl@gmail.com>
2020-07-15 19:31:01 +0100

    feat: Create Order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from products.models import Product
 
+User = get_user_model()
 # Create your models here.
+
+class Order(models.Model):
+    user = models.OneToOneField(User, related_name='orders', on_delete=models.CASCADE)
+    items = models.ManyToManyField(Product)
+    ordered_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f'{self.user.username}\'s order'
