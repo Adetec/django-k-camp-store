3cb2df69fb7362878664c77f6c195c452dd20ef5
Adel Lassag <adetech.aadl@gmail.com>
2020-07-15 18:39:01 +0100

    feat: Add Cart model

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from products.models import Product
 
+
+User = get_user_model()
 # Create your models here.
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, related_name='cart', on_delete=models.CASCADE)
+    items = models.ManyToManyField(Product)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username}\'s cart'
