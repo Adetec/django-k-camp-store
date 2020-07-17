61f471f394ed2c936ceedfa8e52c6234ac2b3959
Adel Lassag <adetech.aadl@gmail.com>
2020-07-17 19:02:13 +0100

    feat: Create cart views (CRUD)

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from products.models import Product
+from .models import Cart
+from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
+
 
 # Create your views here.
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    cart = Cart.objects.get(user= request.user)
+
+    cart.items.add(product)
+
+    return redirect('cart')
+
+
+@login_required
+def remove_from_cart(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    cart = Cart.objects.get(user= request.user)
+
+    cart.items.remove(product)
+
+    return redirect('cart')
+
+
+@login_required
+def remove_all_from_cart(request): 
+    cart = request.user.cart
+    cart.items.clear()
+
+    return redirect('cart')
+
+
+@login_required
+def cart(request):
+    user = request.user
+    products = user.cart.items.all()
+
+    total_price = products.aggregate(Sum('price'))
+    context = {
+        'products': products,
+        'total_price': total_price
+    }
+
+    return render(request, 'carts/cart.html', context)
