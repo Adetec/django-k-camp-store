d3356d167bb3b35fd7390070bd1266a57e7f7b25
Adel Lassag <adetech.aadl@gmail.com>
2020-07-18 04:42:30 +0100

    feat: Create send order email function

---
+++ orders/utils.py
@@ -0,0 +1,17 @@
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+
+def send_confirmation_email(order, total_price):
+    
+    print(order.user.username)
+    subject = f'Thank you {order.user.username} for choosing our products'
+    message = render_to_string(
+        'orders/order-email-details.html',
+        {
+            'order': order,
+            'products': order.items.all(),
+            'total_price': total_price
+        }
+    )
+
+    order.user.email_user(subject, message)
