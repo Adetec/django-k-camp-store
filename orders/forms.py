1899fe9009f42acaf01a7bede4d7bcba49a5a69a
Adel Lassag <adetech.aadl@gmail.com>
2020-07-18 03:31:50 +0100

    feat: Create order's form

---
+++ orders/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = (
+            'address',
+            )
