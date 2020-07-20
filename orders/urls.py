765bf084c300536a1c243b62fec67f57029634d3
Adetec <adetech.aadl@gmail.com>
2020-07-20 19:43:11 +0100

    feat: Add order list url

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import checkout
+from .views import checkout, order_list
 
 urlpatterns = [
+    path('orders/', order_list, name='order_list'),
     path('orders/<int:pk>', checkout, name='checkout'),
 ]
