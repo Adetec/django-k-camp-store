eaa7c1b86f9b305eb8aafccd9215895ec4374797
Adetec <adetech.aadl@gmail.com>
2020-07-20 19:31:52 +0100

    fix: Change orders url

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import checkout
 
 urlpatterns = [
-    path('order/<int:pk>', checkout, name='checkout'),
+    path('orders/<int:pk>', checkout, name='checkout'),
 ]
